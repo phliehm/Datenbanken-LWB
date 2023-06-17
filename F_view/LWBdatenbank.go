40f35b0b9b19eb2cace1d8738d69756b01944598
SchMeb <mebninja@gmail.com>
2023-06-17 20:35:56 +0200

    ...

--- F_view/LWBdatenbank.go
+++ F_view/LWBdatenbank.go
@@ -33,10 +33,11 @@ minNote, maxNote, minPunkte,maxPunkte
 
 var Mutex sync.Mutex					// erstellt Mutex
 	
-var Katknoepfe, Hinzuknoepfe []buttons.Button		// Slices für alle erstellten Knöpfe / die Suchfelder
+var BuZurueck,BuEnde buttons.Button				// Spezille Knoepfe
+var KatKnoepfe, HinzuKnoepfe, VeranstKnoepfe, SpielstKnoepfe []buttons.Button		// Slices für alle erstellten Knöpfe / die Suchfelder
 var DurchsucheFeld felder.Feld
-var VeranstaltungFelder, MinispielFelder []felder.Feld
-var BuZurueck,BuEintrag,BuEnde, BuSuche buttons.Button
+var VeranstFelder, SpielstFelder, VeranstHinzuFelder, HighscoreFelder []felder.Feld
+
 var Akt bool = true							// True gdw. Raum gewechselt wurde _----- NICHT BENÖTIGT?!
 var Anfrage, Suchwort string							// Durchsuchen/Suchwort-String
 var Raumnummer uint8						// Raumnummer des momentanen Raumes
@@ -114,30 +115,44 @@ func main () {
 }
 
 func ErstelleTexte() {
-	Katknopftexte = append(Katknopftexte, "", "Veranstaltungen", "Highscores", "Dummy", "LWB-Übersicht")
-	Hinzuknopftexte = append(Hinzuknopftexte, "Veranstaltung hinzufügen", "Minispiel hinzufügen", "SWS hinzufügen", "Raum hinzufügen","Dozent/in hinzufügen")
-	VeranstaltungFeldtexte = append(VeranstaltungFeldtexte, "Vorlesung", "Thema", "SWS", "Raum","Dozent/in")
+	Katknopftexte = append(Katknopftexte, "", "Veranstaltungen", "  Spielstände", "   Dummy", " LWB-Übersicht", " Aufgaben", " freie SQL-Anfrage", " Neuer Listen-Eintrag")
+	Hinzuknopftexte = append(Hinzuknopftexte, "       Neue Veranstaltung hinzufügen", "       Neues  -> Minispiel <-  hinzufügen", "  SWS hinzufügen", "  Raum hinzufügen","Dozent/in hinzufügen")
+	VeranstaltungFeldtexte = append(VeranstaltungFeldtexte, "NEUE Veranstaltung", "Thema", "SWS", "Raum","Dozent/in")
 }
 
 func ErstelleKnoepfe() {
-	BuSuche		= buttons.New(100,110,500,50, 0,255,0, true, 	"   Durchsuche")			// durchsuche
-	BuZurueck 	= buttons.New(10,620,200,70, 230,50,100, true, 	" zurück")				// zurück
-	BuEintrag 	= buttons.New(400,620,350,70, 230,50,100, true, " Erstelle Eintrag")	// Erstelle Eintrag
-	BuEnde 		= buttons.New(1000,620,180,70, 230,50,100, true," Beenden")				// Beenden
+	BuZurueck 	= buttons.New(20,20,200,70, 230,50,100, true, 	" zurück")					// zurück
+	BuEnde 		= buttons.New(1000,620,180,70, 230,50,100, true," Beenden")					// Beenden
 	
-	Katknoepfe = append(Katknoepfe,
+	KatKnoepfe = append(KatKnoepfe,
 				buttons.New(100,150,300,70, 230,50,100, true, Katknopftexte[1]),
 				buttons.New(100,250,300,70, 230,50,100, true, Katknopftexte[2]),		
 				buttons.New(100,350,300,70, 230,50,100, true, Katknopftexte[3]),		
-				buttons.New(100,450,300,70, 230,50,100, true, Katknopftexte[4])	)
+				buttons.New(100,450,300,70, 230,50,100, true, Katknopftexte[4]),
+				buttons.New(620,150,330,130, 100,230,50, true, Katknopftexte[5]),
+				buttons.New(550,320,500,100, 50,100,230, true, Katknopftexte[6]),
+				buttons.New(500,450,600,100, 130,150,100, true, Katknopftexte[7])   )
 	
 	
-	Hinzuknoepfe = append(Hinzuknoepfe,
-				buttons.New(100,120,1000,70, 230,50,100, true, Hinzuknopftexte[0]),		// 
-				buttons.New(100,220,1000,70, 230,50,100, true, Hinzuknopftexte[1]),		// 
-				buttons.New(100,320,1000,70, 230,50,100, true, Hinzuknopftexte[2]),		// 
-				buttons.New(100,420,1000,70, 230,50,100, true, Hinzuknopftexte[3]),		// 
-				buttons.New(100,520,1000,70, 230,50,100, true, Hinzuknopftexte[4])	)	// 
+	HinzuKnoepfe = append(HinzuKnoepfe,
+				buttons.New(20,120,1160,70, 230,50,100, true, Hinzuknopftexte[0]),		// 
+				buttons.New(20,220,1160,70, 230,50,100, true, Hinzuknopftexte[1]),		// 
+				buttons.New(20,320,1160,70, 230,50,100, true, Hinzuknopftexte[2]),		// 
+				buttons.New(20,420,1160,70, 230,50,100, true, Hinzuknopftexte[3]),		// 
+				buttons.New(20,520,1160,70, 230,50,100, true, Hinzuknopftexte[4])	)	// 
+	
+	SpielstKnoepfe = append(SpielstKnoepfe,
+				buttons.New(20,105,350,55, 0,255,0, true, 	"Spielstände durchsuchen"),
+				buttons.New(390,105,290,55, 0,255,0, true, 	"Highscores anzeigen"),
+				buttons.New(690,105,210,55, 0,255,0, true, 	" Notenbereich"),
+				buttons.New(910,105,220,55, 0,255,0, true, 	" Punktebereich"),
+				BuZurueck )
+	
+	VeranstKnoepfe = append(VeranstKnoepfe,
+				buttons.New(20,105,450,55, 0,255,0, true, 	"  Veranstaltungen durchsuchen"),
+				buttons.New(520,105,290,55, 0,255,0, true, 	"   Eintrag ändern"),
+				buttons.New(860,105,290,55, 0,255,0, true, 	"  Eintrag löschen"),
+				BuZurueck )
 }
 
 func ErstelleFelder() {
@@ -145,48 +160,60 @@ func ErstelleFelder() {
 	
 	DurchsucheFeld = felder.New (110,  110, 30, 'l', "Durchsuche")		// durchsuchen
 	
-	VeranstaltungFelder = append( VeranstaltungFelder,
+	VeranstFelder = append( VeranstFelder,
+		felder.New (110,  110, 30, 'l', "Durchsuche Veranstaltungen"),	
+		felder.New (80, 110, 30, 'l', "Bestehende Veranstaltung"),
+		felder.New (590, 110, 6, 'l', "Neue Raumnummer"),
+		felder.New (720, 110, 25, 'l', "Neue/r Dozent/in")	)
+		
+	SpielstFelder = append( SpielstFelder,
+		felder.New (110,  110, 30, 'l', "Durchsuche Spielstände"),	
+		felder.New (80, 110, 30, 'l', "minPunkte"),
+		felder.New (590, 110, 6, 'l', "maxPunkte"),
+		felder.New (720, 110, 25, 'l', "minNote"),
+		felder.New (720, 110, 25, 'l', "maxNote")	)
+	
+	VeranstHinzuFelder = append( VeranstHinzuFelder,
 		felder.New (20, 160, 25, 'l', VeranstaltungFeldtexte[0]),	
 		felder.New (450, 160, 20, 'l', VeranstaltungFeldtexte[1]),
 		felder.New (770, 160, 3, 'l', VeranstaltungFeldtexte[2]),
 		felder.New (820, 160, 4, 'l', VeranstaltungFeldtexte[3]),
 		felder.New (880, 160, 15, 'l', VeranstaltungFeldtexte[4])	)
 }
-func ZeichneKatknoepfe() {
-	for _,bu := range Katknoepfe {
+func ZeichneKatKnoepfe() {
+	for _,bu := range KatKnoepfe {
 		bu.ZeichneButton()
 	}
 }
-func AktiviereKatknoepfe() {
-	for _,bu := range Katknoepfe {
+func AktiviereKatKnoepfe() {
+	for _,bu := range KatKnoepfe {
 		bu.AktiviereButton()
 	}
 }
-func DeaktiviereKatknoepfe() {
-	for _,bu := range Katknoepfe {
+func DeaktiviereKatKnoepfe() {
+	for _,bu := range KatKnoepfe {
 		bu.DeaktiviereButton()
 	}
 }
-func ZeichneHinzuknoepfe() {
-	for _,bu := range Hinzuknoepfe {
+func ZeichneHinzuKnoepfe() {
+	for _,bu := range HinzuKnoepfe {
 		bu.ZeichneButton()
 	}
 }
-func AktiviereHinzuknoepfe() {
-	for _,bu := range Hinzuknoepfe {
+func AktiviereHinzuKnoepfe() {
+	for _,bu := range HinzuKnoepfe {
 		bu.AktiviereButton()
 	}
 }
-func DeaktiviereHinzuknoepfe() {
-	for _,bu := range Hinzuknoepfe {
+func DeaktiviereHinzuKnoepfe() {
+	for _,bu := range HinzuKnoepfe {
 		bu.DeaktiviereButton()
 	}
 }
 
 func ZeichneRaum() {
-	DeaktiviereKatknoepfe()
-	DeaktiviereHinzuknoepfe()
-	BuSuche.DeaktiviereButton()
+	DeaktiviereKatKnoepfe()
+	DeaktiviereHinzuKnoepfe()
 
 	UpdateAus () 										// Nun wird alles im nicht sichtbaren "hinteren" Fenster gezeichnet!
 	Stiftfarbe(255,255,255)
@@ -197,21 +224,18 @@ func ZeichneRaum() {
 	switch Raumnummer {
 		case 0:	
 		SchreibeFont(300,10,"LWB - Datenbank")
-		AktiviereKatknoepfe()
-		ZeichneKatknoepfe()
-		BuEintrag.ZeichneButton()
+		AktUndZeichne(KatKnoepfe)
+		
 		BuEnde.ZeichneButton()
 		case 1:
 		SchreibeFont(300,10,Katknopftexte[1])
-		BuSuche.AktiviereButton() 
-		BuSuche.ZeichneButton()
+		AktUndZeichne(VeranstKnoepfe)
 		
 		textboxTabelle.ZeichneAnfrage(conn,sucheDozVer(""),20,170,true,0,0,0,0,0,255,16,font)
 		BuZurueck.ZeichneButton()
 		case 2:
 		SchreibeFont(300,10,Katknopftexte[2])
-		BuSuche.AktiviereButton() 
-		BuSuche.ZeichneButton()
+		AktUndZeichne(SpielstKnoepfe)
 		
 		textboxTabelle.ZeichneAnfrage(conn,sucheSpielerGamesScores(""),20,170,true,0,0,0,0,0,255,16,font)
 		BuZurueck.ZeichneButton()
@@ -221,13 +245,26 @@ func ZeichneRaum() {
 		case 4:
 		SchreibeFont(300,10,Katknopftexte[4])
 		BuZurueck.ZeichneButton()
-		case 10:										// Eintrag hinzufügen
+		case 8:											// --> 	Aufgaben
+		SchreibeFont(300,10,Katknopftexte[5])
+		BuZurueck.ZeichneButton()
+		case 9:											// --> freie SQL-Anfrage
+		SchreibeFont(300,10,Katknopftexte[6])
+		BuZurueck.ZeichneButton()
+		case 10:										// --> Eintrag hinzufügen
 		SchreibeFont(250,10,"Eintrag hinzufügen")
-		AktiviereHinzuknoepfe()
-		ZeichneHinzuknoepfe()
+		AktiviereHinzuKnoepfe()
+		ZeichneHinzuKnoepfe()
 		BuZurueck.ZeichneButton()
 	}
 	UpdateAn () 										// Nun wird der gezeichnete Frame sichtbar gemacht!
+}
+
+func AktUndZeichne(knoepfe []buttons.Button) {
+	for _,knopf := range knoepfe {
+		knopf.AktiviereButton()
+		knopf.ZeichneButton()
+	}
 }
 
 // Es folgt die Maus-Komponente 1 --- Kein Bestandteil der Welt, also unabhängig -----
@@ -240,68 +277,158 @@ func maussteuerung () {
 		if status==1 { 													// Maustaste gedrückt
 			
 			// ------------------------------- RAUM-WECHSEL ---------------------------------------------------- AB HIER
-			for _,knopf := range Katknoepfe { 									// überprüft Knöpfe im Array
+			for _,knopf := range KatKnoepfe { 									// überprüft Knöpfe im Array
 				if knopf.TesteXYPosInButton(mausX,mausY) {
 					fmt.Println("Knopf gedrückt: ", knopf.GibBeschriftung() )
 					switch knopf.GibBeschriftung() {
 						case Katknopftexte[1]: Raumnummer = 1
 						case Katknopftexte[2]: Raumnummer = 2						
 						case Katknopftexte[3]: Raumnummer = 3
-						case Katknopftexte[4]: Raumnummer = 4						
+						case Katknopftexte[4]: Raumnummer = 4
+						case Katknopftexte[5]: Raumnummer = 8
+						case Katknopftexte[6]: Raumnummer = 9
+						case Katknopftexte[7]: Raumnummer = 10
 					}
 					ZeichneRaum()					// Raum wurde gewechselt und muss neu gezeichnet werden
 				}
 			}
-			if BuZurueck.TesteXYPosInButton(mausX,mausY) {
+			if BuZurueck.TesteXYPosInButton(mausX,mausY) {					// Zurück-Botton gedrückt
 				Raumnummer = 0
-				ZeichneRaum()					// Raum wurde gewechselt und muss neu gezeichnet werden
-			} else if BuEintrag.TesteXYPosInButton(mausX,mausY) {
-				Raumnummer = 10
 				ZeichneRaum()					// Raum wurde gewechselt und muss neu gezeichnet werden
 			}
 			// ------------------------------- RAUM-WECHSEL ---------------------------------------------------- BIS HIER
 			
 			// ------------------------------- KNOPF-FUNKTIONEN ------------------------------------------------ AB HIER
 			Stiftfarbe(255,255,255)
-			if BuSuche.TesteXYPosInButton(mausX,mausY) {
-				Vollrechteck(100,110,500,60)
-				Suchwort = DurchsucheFeld.Edit()
-				
-				Stiftfarbe(255,255,255)
-				Vollrechteck(20,170,1180,530)
-				BuZurueck.ZeichneButton()
-				
-				switch Raumnummer {
-					case 1: Anfrage = sucheDozVer(Suchwort)
-					case 2: Anfrage = sucheSpielerGamesScores(Suchwort)
+			switch Raumnummer {
+				case 1:																					// ------------------ Veranstaltungen-Raum
+				if VeranstKnoepfe[0].TesteXYPosInButton(mausX,mausY) {				// -- Durchsuche die Liste
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[0].Edit()
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,170,1180,530)
+					
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(VeranstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				} else if VeranstKnoepfe[1].TesteXYPosInButton(mausX,mausY) {		// -- Eintrag ändern
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[1].Edit()
+					VeranstFelder[2].Edit()
+					VeranstFelder[3].Edit()
+					
+					// ----------------------------------- HIER fehlt die SQL-Anfrage zum Ändern des Eintrags
+					
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(VeranstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				} else if VeranstKnoepfe[2].TesteXYPosInButton(mausX,mausY) {		// -- Eintrag löschen
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[1].Edit()
+									
+					// ----------------------------------- HIER fehlt die SQL-Anfrage zum Löschen des Eintrags
+					
+					Suchwort = ""
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(VeranstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
 				}
-				textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
-			}
-			for _,suchknopf := range Hinzuknoepfe { 									// überprüft SUCHKNÖPFE im Array
-				if suchknopf.TesteXYPosInButton(mausX,mausY) {
-					fmt.Println("Einfüge-Knopf gedrückt: ", suchknopf.GibBeschriftung() )
-					switch suchknopf.GibBeschriftung() {
-						case Hinzuknopftexte[0]: 
-						Vollrechteck(100,120,1000,70)
-						
-						for _,feldwert := range VeranstaltungFelder {
-							feldwert.Edit()
-						}
-						case Hinzuknopftexte[1]:	
-						
-						case Hinzuknopftexte[2]: 
-						Vollrechteck(590,150,80,50)
-						
-						case Hinzuknopftexte[3]:	
-						Vollrechteck(700,150,80,50)
-						
-						case Hinzuknopftexte[4]:	
-						Vollrechteck(850,150,150,50)
+				case 2: 																					// ------------------ Spielstände-Raum
+				if VeranstKnoepfe[0].TesteXYPosInButton(mausX,mausY) {				// -- Durchsuche die Liste
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[0].Edit()
+					Anfrage = sucheSpielerGamesScores(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,170,1180,530)
+					
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(SpielstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				} else if VeranstKnoepfe[1].TesteXYPosInButton(mausX,mausY) {		// -- Highscores
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[1].Edit()
+					VeranstFelder[2].Edit()
+					VeranstFelder[3].Edit()
+					
+					// ----------------------------------- HIER fehlt die SQL-Anfrage zum Ändern des Eintrags
+					
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(SpielstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				} else if VeranstKnoepfe[2].TesteXYPosInButton(mausX,mausY) {		// -- Notenbereich eingeben
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[1].Edit()
+									
+					// ----------------------------------- HIER fehlt die SQL-Anfrage zum Löschen des Eintrags
+					
+					Suchwort = ""
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(SpielstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				} else if VeranstKnoepfe[3].TesteXYPosInButton(mausX,mausY) {		// -- Punktebereich eingeben
+					Vollrechteck(20,105,1160,60)
+					Suchwort = VeranstFelder[1].Edit()
+									
+					// ----------------------------------- HIER fehlt die SQL-Anfrage zum Löschen des Eintrags
+					
+					Suchwort = ""
+					Anfrage = sucheDozVer(Suchwort)
+					
+					Stiftfarbe(255,255,255)
+					Vollrechteck(20,105,1160,60)
+					AktUndZeichne(SpielstKnoepfe)
+					
+					textboxTabelle.ZeichneAnfrage(conn,Anfrage,20,170,true,0,0,0,0,0,255,16,font)
+				}
+			
+				case 10:
+				for _,suchknopf := range HinzuKnoepfe { 									// überprüft SUCHKNÖPFE im Array
+					if suchknopf.TesteXYPosInButton(mausX,mausY) {
+						fmt.Println("Einfüge-Knopf gedrückt: ", suchknopf.GibBeschriftung() )
+						switch suchknopf.GibBeschriftung() {
+							case Hinzuknopftexte[0]: 
+							Vollrechteck(100,120,1000,70)
 							
+							for _,feldwert := range VeranstHinzuFelder {
+								feldwert.Edit()
+							}
+							case Hinzuknopftexte[1]:	
+							
+							case Hinzuknopftexte[2]: 
+							Vollrechteck(590,150,80,50)
+							
+							case Hinzuknopftexte[3]:	
+							Vollrechteck(700,150,80,50)
+							
+							case Hinzuknopftexte[4]:	
+							Vollrechteck(850,150,150,50)
+								
+						}
 					}
 				}
+				
 			}
-			
 		}
 	}
 }
